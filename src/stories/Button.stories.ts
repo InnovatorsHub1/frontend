356ddb0947ae54b5c55e11575542aceaae5bfc0c
benshabbat <benshabbat27@gmail.com>
2025-01-23 12:01:06 +0200

    added all stories

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,30 +1,151 @@
-import GenericButton from "@src/components/Button/GenericButton";
-import { Meta, StoryObj } from "@storybook/react";
-
-import { ComponentProps } from "react";
+import GenericButton from '@src/components/Button/GenericButton';
+import { Meta, StoryObj } from '@storybook/react';
+import { Delete } from '@mui/icons-material';
+import { ComponentProps } from 'react';
 
 type StoryProps = ComponentProps<typeof GenericButton>;
 
-const meta : Meta<StoryProps> = {
-    component: GenericButton
-}
+const meta: Meta<StoryProps> = {
+  component: GenericButton,
+};
 
 export default meta;
 
 type Story = StoryObj<StoryProps>;
 
 export const primary: Story = {
-    args:{
-        variant: 'primary',
-        children:"primary"
-    }
-}
+  args: {
+    variant: 'primary',
+    children: 'primary',
+  },
+};
+
+export const secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'secondary',
+  },
+};
+
+export const outlined: Story = {
+  args: {
+    variant: 'outlined',
+    children: 'outlined',
+  },
+};
+
+export const text: Story = {
+  args: {
+    variant: 'text',
+    children: 'text',
+  },
+};
+
+export const link: Story = {
+  args: {
+    variant: 'link',
+    children: 'link',
+  },
+};
+
+export const SmallSize: Story = {
+  args: {
+    size: 'sm',
+    children: 'SmallSize',
+  },
+};
+
+export const MediumSize: Story = {
+  args: {
+    size: 'md',
+    children: 'MediumSize',
+  },
+};
+
+export const LargeSize: Story = {
+  args: {
+    size: 'lg',
+    children: 'LargeSize',
+  },
+};
+
+export const loading: Story = {
+  args: {
+    loading: true,
+    children: 'loading',
+  },
+};
 
 export const activeButton: Story = {
-    args:{
-        isActive: true,
-        isActiveAcolor: 'red',
-        isActiveBackgroundColor: 'pink',
-        children:"Active Button"
-    }
-}
+  args: {
+    isActive: true,
+    isActiveAcolor: 'red',
+    isActiveBackgroundColor: 'pink',
+    children: 'Active Button',
+  },
+};
+
+export const disabled: Story = {
+  args: {
+    disabled: true,
+    children: 'Disabled',
+  },
+};
+
+export const fullWidth: Story = {
+  args: {
+    fullWidth: true,
+    children: 'Full Width',
+  },
+};
+
+export const IconLeft: Story = {
+  args: {
+    iconPosition: 'left',
+    icon: "🚀",
+    children: 'IconLeft',
+  },
+};
+
+export const IconRight: Story = {
+  args: {
+    iconPosition: 'right',
+    icon: "🚀",
+    children: 'IconRight',
+  },
+};
+
+export const padding: Story = {
+  args: {
+    padding: '24px',
+    children: 'padding 24px',
+  },
+};
+
+export const fontSize: Story = {
+  args: {
+    fontSize: '24px',
+    children: 'fontSize 24px',
+  },
+};
+
+export const margin: Story = {
+  args: {
+    margin: '24px',
+    children: 'margin 24px',
+  },
+};
+
+export const backgroundColor: Story = {
+  args: {
+    backgroundColor: 'green',
+    children: 'backgroundColor green',
+  },
+};
+
+export const color: Story = {
+  args: {
+    color: 'red',
+    children: 'color red',
+  },
+};
