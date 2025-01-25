e8a4f1880012663cd271cc052e31351e531a361c
Paz Gueta <pazgueta@gmail.com>
2025-01-25 15:53:37 +0200

    handle merge conflicts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,78 @@
 import { Button } from '@mui/material';
 import Modal from '@src/components/Modal/Modal';
-import React, { useState } from 'react';
-const HomePage = () => {
+import { useState } from 'react';
+
+import GenericButton from '@src/components/Button/GenericButton';
+import { Delete } from '@mui/icons-material';
+export default function HomePage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
   return (
-    <>
-      <div className='size-full p-6'>
-        <div>Main</div>
-        <div>window</div>
-        <Button variant='contained'>Contained</Button>
-        <Button variant='contained' disabled>
-          Disabled
-        </Button>
-        <Button variant='contained' href='#contained-buttons'>
-          Link
-        </Button>
-      </div>
+    <div className='size-full p-6'>
+      <div>Main</div>
+      <div>window</div>
+
+      <GenericButton
+        variant='primary'
+        size='sm'
+        icon={
+          <Delete
+            sx={{
+              color: 'blue',
+              width: '72px', // Explicitly set width
+              height: '72px', // Explicitly set height
+            }}
+          />
+        }
+        iconPosition='left'
+        padding='24px'
+        fontSize='24px'
+        margin={20}
+        type='button'
+        isActive={true}
+        backgroundColor='green'
+        color='red'
+      >
+        Test222
+      </GenericButton>
+      <GenericButton
+        variant='primary'
+        size='sm'
+        icon={<Delete />}
+        iconPosition='left'
+        padding='24px'
+        fontSize='24px'
+        margin={20}
+        type='button'
+      >
+        Test123
+      </GenericButton>
+      <GenericButton
+        variant='primary'
+        size='sm'
+        icon={<Delete />}
+        iconPosition='left'
+        padding='24px'
+        fontSize='24px'
+        margin={20}
+        type='button'
+      >
+        Test222345
+      </GenericButton>
+
+      <GenericButton>Test</GenericButton>
+
+      <br />
+      <Button variant='contained'>Contained</Button>
+      <Button variant='contained' disabled>
+        Disabled
+      </Button>
+      <Button variant='contained' href='#contained-buttons'>
+        Link
+      </Button>
       <div>
         <button onClick={openModal}>Open Modal</button>
         <Modal
@@ -32,8 +85,6 @@ const HomePage = () => {
           <p>This is a test modal. It works!</p>
         </Modal>
       </div>
-    </>
+    </div>
   );
-};
-
-export default HomePage;
+}
