94a3b020fdf5c3e1d62e51d2f525297084893eb6
benshabbat <benshabbat27@gmail.com>
2025-02-04 15:36:35 +0200

    fix problem

--- src/components/form/Select/Select.tsx
+++ src/components/form/Select/Select.tsx
@@ -1,47 +1,64 @@
 import { Autocomplete, TextField } from '@mui/material';
 import { useMemo } from 'react';
 
-type SelectOption = {
-    id?: number;
-    label: string;
+export type SelectOption = {
+  id?: number; 
+  label: string;
+  group?: string;
 };
 
 interface SelectProps {
-    options: SelectOption[];
-    value?: SelectOption | SelectOption[];
-    onChange?: (value: SelectOption | SelectOption[]) => void;
-    multiple?: boolean;
-    groupBy?: (option: SelectOption) => string;
-    renderOption?: (props: React.HTMLAttributes<HTMLLIElement>, option: SelectOption) => React.ReactNode;
-    placeholder?: string;
+  options: SelectOption[];
+  value: SelectOption | SelectOption[] | null;
+  onChange: (value: SelectOption | SelectOption[] | null) => void;
+  multiple?: boolean;
+  groupBy?: (option: SelectOption) => string;
+  renderOption?: (props: React.HTMLAttributes<HTMLLIElement>, option: SelectOption) => React.ReactNode;
+  placeholder?: string;
 }
 
-
-
 export default function Select({
-    options,
-    value,
-    onChange,
-    multiple = false,
-    groupBy,
-    renderOption,
-    placeholder = "Search...",
+  options,
+  value,
+  onChange,
+  multiple,
+  groupBy,
+  renderOption,
+  placeholder = 'Search...',
 }: SelectProps) {
-    return (
-        <Autocomplete
-            disablePortal
-            options={options}
-            value={value}
-            onChange={() => onChange}
-            multiple={multiple}
-            groupBy={groupBy}
-            sx={{ width: 300 }}
-            renderOption={(props, option) => (
-                <li {...props} key={option.id}>
-                    {renderOption ? renderOption(props, option) : option.label}
-                </li>
-            )}
-            renderInput={(params) => <TextField {...params} label={placeholder} />}
-        />
-    );
+  const sortedOptions = useMemo(() => {
+    if (!groupBy) return options;
+
+    return [...options].sort((a, b) => {
+      const groupA = groupBy(a) || '';
+      const groupB = groupBy(b) || '';
+
+      const groupCompare = groupA.localeCompare(groupB);
+      if (groupCompare !== 0) return groupCompare;
+
+      return a.label.localeCompare(b.label);
+    });
+  }, [options, groupBy]);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: SelectOption | SelectOption[] | null) => {
+    onChange(newValue);
+  };
+
+  return (
+    <Autocomplete
+      disablePortal
+      options={sortedOptions}
+      value={value}
+      onChange={handleChange}
+      multiple={multiple}
+      groupBy={groupBy}
+      sx={{ width: 300 }}
+      renderOption={(props, option) => (
+        <li {...props} key={option.id}>
+          {renderOption ? renderOption(props, option) : option.label}
+        </li>
+      )}
+      renderInput={(params) => <TextField {...params} label={placeholder} />}
+    />
+  );
 }
