ee4dd1f88f8629115e30210149ce8bba2c3dc34a
benshabbat <benshabbat27@gmail.com>
2025-02-06 12:00:27 +0200

    the flow of classname fixed bc clsx

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,28 +1,34 @@
-import clsx from 'clsx';
+// import clsx from 'clsx';
 import styles from './Link.module.scss';
 import { Link as ReactLink } from 'react-router-dom';
 
 export type ILinkProps = {
   children: React.ReactNode;
-  type: 'primary' | 'secondary' | 'default';
+  type: 'primary' | 'secondary';
   className?: string;
   to?: string;
 };
 
 export default function Link(props: ILinkProps) {
   const { className, children, to = '', type } = props;
-  const linkClass = clsx(
-    {
-      'text-primary dark:dark-text-primary': type === 'primary',
-      'text-secondary dark:dark-text-secondary': type === 'secondary',
-      'text-default dark:dark-text-default': type === 'default',
-    },
-    className,
-  );
+  const linkClass =
+    type === 'primary'
+      ? 'text-primary dark:dark-text-primary'
+      : type === 'secondary'
+        ? 'text-secondary dark:dark-text-secondary'
+        : 'text-default dark:dark-text-default';
+  // const linkClass = clsx(
+  //   {
+  //     'text-primary dark:dark-text-primary': type === 'primary',
+  //     'text-secondary dark:dark-text-secondary': type === 'secondary',
+  //     'text-default dark:dark-text-default': type === 'default',
+  //   },
+  //   className,
+  // );
 
   return (
     <div className={styles.linkWrapper}>
-      <ReactLink to={to} className={linkClass}>
+      <ReactLink to={to} className={className ?? linkClass}>
         {children}
       </ReactLink>
     </div>
