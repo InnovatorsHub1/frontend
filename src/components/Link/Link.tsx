3a9f1bce9b5cc92023d83b3340725a0334d53f93
Shay Bushary <shaybush93@gmail.com>
2025-02-06 11:45:21 +0200

    added type

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,19 +1,30 @@
+import clsx from 'clsx';
 import styles from './Link.module.scss';
+import { Link as ReactLink } from 'react-router-dom';
 
 export type ILinkProps = {
   children: React.ReactNode;
+  type: 'primary' | 'secondary' | 'default';
   className?: string;
   to?: string;
 };
 
 export default function Link(props: ILinkProps) {
-  const { className, children, to } = props;
+  const { className, children, to = '', type } = props;
+  const linkClass = clsx(
+    {
+      'text-primary dark:dark-text-primary': type === 'primary',
+      'text-secondary dark:dark-text-secondary': type === 'secondary',
+      'text-default dark:dark-text-default': type === 'default',
+    },
+    className,
+  );
 
   return (
-    <div className={styles.link}>
-      <Link to={to} className={className}>
+    <div className={styles.linkWrapper}>
+      <ReactLink to={to} className={linkClass}>
         {children}
-      </Link>
+      </ReactLink>
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import Link from '@src/components/Link';
 export default function HomePage() {
   return (
     <div className='size-full p-6'>
-      <Link to='/redux' className='text-primary dark:text-dark-card'>
-        Hello
+      <Link to='/redux' className='text-red-500' type='secondary'>
+        Hello11111§111
       </Link>
       <div className='bg-error dark:bg-dark-primary'>error</div>
     </div>
