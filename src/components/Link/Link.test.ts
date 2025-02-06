34bed78876ce0c0e691e9afa6edd86c1b14f55d7
Shay Bushary <shaybush93@gmail.com>
2025-02-06 13:48:37 +0200

    should implement the tests

--- src/components/Link/Link.test.ts
+++ src/components/Link/Link.test.ts
@@ -1,18 +1,18 @@
 import '@testing-library/jest-dom';
 
 describe('Link Component', () => {
-    // beforeEach
-    // afterEach
+  // beforeEach
+  // afterEach
 
-    // 1 - if the user send prop type=primary check if there is class text-primary
-    // 2 - if the user send prop to and click on the button make sure it navigate to the right place 
-    test('should fit className by the provided type primary', () => {
-        // TODO - add 
-    });
+  // 1 - if the user send prop type=primary check if there is class text-primary
+  // 2 - if the user send prop to and click on the button make sure it navigate to the right place
+  test('should fit className by the provided type primary', () => {
+    // TODO - add
+  });
 
-    test('should naviate to the right place', () => {
-        // TODO - add
-    })
+  test('should naviate to the right place', () => {
+    // TODO - add
+  });
 
-    // pnpm run test:ci -> should pass all the tests
+  // pnpm run test:ci -> should pass all the tests
 });
