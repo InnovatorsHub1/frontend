29bb9796444c9da4c6946330d45a8be8b8466558
benshabbat <benshabbat27@gmail.com>
2025-02-09 09:51:44 +0200

    just need tests

--- src/components/Link/Link.test.ts
+++ src/components/Link/Link.test.ts
@@ -1,18 +1,34 @@
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import Link from './Link';
 
-describe('Link Component', () => {
-  // beforeEach
-  // afterEach
 
-  // 1 - if the user send prop type=primary check if there is class text-primary
-  // 2 - if the user send prop to and click on the button make sure it navigate to the right place
-  test('should fit className by the provided type primary', () => {
-    // TODO - add
+describe('Sidebar Component', () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Link />
+      </BrowserRouter>,
+    );
   });
+// describe('Link Component', () => {
+//   // beforeEach
+//   // afterEach
 
-  test('should naviate to the right place', () => {
-    // TODO - add
-  });
+//   // 1 - if the user send prop type=primary check if there is class text-primary
+//   // 2 - if the user send prop to and click on the button make sure it navigate to the right place
+//   test('should fit className by the provided type primary', () => {
+//     // TODO - add
+//   });
 
-  // pnpm run test:ci -> should pass all the tests
+//   test('should naviate to the right place', () => {
+//     // TODO - add
+//   });
+
+//   // pnpm run test:ci -> should pass all the tests
 });
+
+
