2003ae43fb5a2e66e1898c18dae22ef863c2e713
benshabbat <benshabbat27@gmail.com>
2025-02-09 11:59:34 +0200

    made textfield component with check in home page try compare with figma for style using tw for style

---
+++ src/components/form/TextField/TextField.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export type TextFieldProps = {
+  className?: string;
+  placeholder?: string;
+  left?: string | number;
+  top?: string | number;
+  width?: string | number;
+  height?: string | number;
+};
+function TextField(propsInput: TextFieldProps) {
+  const [inputValue, setInputValue] = useState<string>('');
+  const { className, left = '34px', top = '61px', width = '298px', height = '60px', ...props } = propsInput;
+  return (
+    <div className='flex flex-col' style={{ left, top, width, height }}>
+      <label className='bg-black text-white'>Email {inputValue ? 'With Info' : 'Empty'}</label>
+      <input
+        {...props}
+        style={{ width: 298, height: 35, top: '25px', borderRadius: '4px', borderWidth: '1px' }}
+        className={`border-2 border-black rounded-md ${className}`}
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default TextField;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,11 @@
+import TextField from "@src/components/form/TextField";
+
 export default function HomePage() {
   return (
     <div className='size-full p-6'>
       <div className='bg-error dark:bg-dark-primary'>error</div>
+      <TextField  placeholder={"Entermail@gmail.com"}  />
     </div>
   );
 }
+
