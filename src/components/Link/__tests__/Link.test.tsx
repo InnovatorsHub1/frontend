a85d4c4ade7bd16dbd44b7aac812de0f72b06476
benshabbat <benshabbat27@gmail.com>
2025-02-11 11:20:16 +0200

    working on test for new reviews

--- src/components/Link/__tests__/Link.test.tsx
+++ src/components/Link/__tests__/Link.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen,fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Link from '../Link';
 import { BrowserRouter } from 'react-router-dom';
 // import userEvent from '@testing-library/user-event';
@@ -9,7 +9,7 @@ describe('Link Component', () => {
     render(
       <BrowserRouter>
         <Link type='primary'>TEST</Link>
-      </BrowserRouter>
+      </BrowserRouter>,
     );
   });
 
@@ -24,39 +24,43 @@ describe('Link Component', () => {
   });
 });
 
-  it('navigates to the correct path when specified', () => {
-    const TEST_PATH = '/redux';
-    render(<Link type='primary' to={TEST_PATH}>TEST</Link>);
-    const linkElement = screen.getByText(/TEST/i);
-    expect(linkElement).toHaveAttribute('href', TEST_PATH);
-  });
+it('navigates to the correct path when specified', () => {
+  const TEST_PATH = '/redux';
+  render(
+    <Link type='primary' to={TEST_PATH}>
+      TEST
+    </Link>,
+  );
+  const linkElement = screen.getByText(/TEST/i);
+  expect(linkElement).toHaveAttribute('href', TEST_PATH);
+});
 
-  it('combines custom className with type classes', () => {
-    const customClass = 'custom-class';
-    render(
-      <Link type='primary' className={customClass}>
-        TEST
-      </Link>
-    );
-    const linkElement = screen.getByText(/TEST/i);
-    expect(linkElement).toHaveClass('text-primary', 'dark:dark-text-primary', customClass);
-  });
+it('combines custom className with type classes', () => {
+  const customClass = 'custom-class';
+  render(
+    <Link type='primary' className={customClass}>
+      TEST
+    </Link>,
+  );
+  const linkElement = screen.getByText(/TEST/i);
+  expect(linkElement).toHaveClass('text-primary', 'dark:dark-text-primary', customClass);
+});
 
-  it('renders children correctly', () => {
-    render(
-      <Link type='primary'>
-        <span data-testid="child">Child Content</span>
-      </Link>
-    );
-    expect(screen.getByTestId('child')).toBeInTheDocument();
-  });
+it('renders children correctly', () => {
+  render(
+    <Link type='primary'>
+      <span data-testid='child'>Child Content</span>
+    </Link>,
+  );
+  expect(screen.getByTestId('child')).toBeInTheDocument();
+});
 
-  it('uses root path "/" when no "to" prop is provided and navigates correctly', async () => {
-    // const user = userEvent.setup();
-    render(<Link type='primary'>TEST</Link>);
-    const linkElement = screen.getByText(/TEST/i);
-    expect(linkElement).toHaveAttribute('href', '/#test');
-    // await user.click(linkElement);
-    fireEvent.click(linkElement);
-    expect(window.location.pathname).toBe('/#test');
-  });
+it('uses root path "/" when no "to" prop is provided and navigates correctly', async () => {
+  // const user = userEvent.setup();
+  render(<Link type='primary'>TEST</Link>);
+  const linkElement = screen.getByText(/TEST/i);
+  expect(linkElement).toHaveAttribute('href', '/#test');
+  // await user.click(linkElement);
+  fireEvent.click(linkElement);
+  expect(window.location.pathname).toBe('/#test');
+});
