238d5d1068f2f2bf82594c2ce29c6c1534c5fabc
benshabbat <benshabbat27@gmail.com>
2025-02-13 18:24:20 +0200

    imporved tests added validation in main component

--- src/components/form/TextField/TextField.tsx
+++ src/components/form/TextField/TextField.tsx
@@ -8,66 +8,103 @@ export type TextFieldProps = {
   type?: string;
 };
 
+const FIELD_ICONS = {
+  email: <EmailOutlined />,
+  'phone number': 'IL |',
+  website: 'http:// |',
+} as const;
+
 function TextField(propsInput: TextFieldProps) {
   const [inputValue, setInputValue] = useState<string>('');
-  const { className, placeholder, name="", type="" } = propsInput;
+  const [isValid, setIsValid] = useState<boolean>(true);
+  const [isTouched, setIsTouched] = useState<boolean>(false);
+  const { className, name, type, placeholder } = propsInput;
 
-  const icons = [
-    { icon: <EmailOutlined />, name: 'email', type: 'email' },
-    { icon: 'IL |', name: 'phone number', type: 'tel' },
-    { icon: 'http:// |', name: 'website', type: 'url' },
-  ];
-  function statusField(status: string) {
-    if (status === 'error') {
-      return 'text-red-500  border-solid border-[2px] border-red-500 ';
+  const handleValidation = (value: string) => {
+    if (type === 'email') {
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
     }
-    if (status === 'success') {
-      return 'text-green-500  border-solid border-[2px] border-green-500 ';
+    if (type === 'url') {
+      return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(value);
     }
-    if (status) {
-      return 'text-black-500';
-    } else {
-      return 'text-gray-500';
+    if (type === 'tel') {
+      return /^[0-9\s+()-]*$/.test(value);
     }
-  }
+    return true;
+  };
 
-  const websiteIput =
-    icons.find((icon) => icon?.name === name?.toLowerCase())?.name === 'website' ||
-    icons.find((icon) => icon?.type === type?.toLowerCase())?.type === 'url';
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+    setIsTouched(true);
+    setIsValid(handleValidation(e.target.value));
+  };
+
+  const handleBlur = () => {
+    setIsTouched(true);
+    setIsValid(handleValidation(inputValue));
+  };
+
+  const getStatusStyle = () => {
+    if (!isTouched) return 'text-gray-500';
+    if (!isValid) return 'text-red-500 border-solid border-[2px] border-red-500';
+    if (inputValue) return 'text-green-500 border-solid border-[2px] border-green-500';
+    return 'text-gray-500';
+  };
+
+  const getHelpText = () => {
+    if (!isValid && isTouched) {
+      switch (type) {
+        case 'email':
+          return 'Please enter a valid email address';
+        case 'url':
+          return 'Please enter a valid URL';
+        case 'tel':
+          return 'Please enter a valid phone number';
+        default:
+          return '';
+      }
+    }
+    return '';
+  };
+
+  const fieldName = name?.toLowerCase();
+  const fieldType = type?.toLowerCase();
+
+  const icon = FIELD_ICONS[fieldName as keyof typeof FIELD_ICONS] || FIELD_ICONS[fieldType as keyof typeof FIELD_ICONS];
+
+  const isWebsite = fieldName === 'website' || fieldType === 'url';
+
   return (
-    <div className='flex flex-col bg-black' style={{ left: '34px', top: '61px', width: '298px', height: '60px' }}>
+    <div className='flex flex-col bg-black left-[34px] top-[61px] w-[298px] h-[60px]'>
       <label className='pl-2 text-white'>{name || type}</label>
+
       {name === 'description' || type === 'textarea' ? (
         <textarea
-          placeholder={placeholder}
           name={name}
-          style={{ width: '298px', height: '130px', top: '25px', borderRadius: '4px' }}
-          className={`${websiteIput ? 'pl-18' : 'pl-10'} pr-10 border-2 border-black rounded-md ${className}`}
+          placeholder={placeholder}
+          className={`w-[298px] h-[130px] top-[25px] rounded-md ${isWebsite ? 'pl-18' : 'pl-10'} pr-10 border-2 border-black ${className}`}
           value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-        ></textarea>
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
       ) : (
-        <div className={`relative ${statusField(inputValue)} `}>
-          <span className='absolute left-2 top-1/2 transform -translate-y-1/2'>
-            {icons.find((icon) => icon.type === props.type?.toLowerCase())?.icon ||
-              icons.find((icon) => icon.name === props.name?.toLowerCase())?.icon}
-          </span>
+        <div className={`relative ${getStatusStyle()}`}>
+          <span className='absolute left-2 top-1/2 transform -translate-y-1/2'>{icon}</span>
           <input
             name={name}
             type={type}
             placeholder={placeholder}
-            className={` ${statusField(inputValue)} ${icons.find((icon) => icon.name === name?.toLowerCase())?.name === 'website' ? 'pl-18' : 'pl-10'} pr-10 border-2 border-black rounded-md ${className}`}
-            style={{ width: '298px', height: '35px', top: '25px', borderRadius: '4px' }}
+            className={`w-[298px] h-[35px] top-[25px] rounded-md ${getStatusStyle()} ${
+              isWebsite ? 'pl-18' : 'pl-10'
+            } pr-10 border-2 border-black ${className}`}
             value={inputValue}
-            onChange={(e) => {
-              setInputValue(e.target.value);
-            }}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
-          <HelpOutline className='absolute right-2 top-1/2 transform -translate-y-1/2' />
+          <HelpOutline className='absolute right-2 top-1/2 transform -translate-y-1/2' title={getHelpText()} />
         </div>
       )}
+      {!isValid && isTouched && <span className='text-red-500 text-sm mt-1'>{getHelpText()}</span>}
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,10 +5,10 @@ export default function HomePage() {
     <div className='size-full p-6'>
       <div className='bg-error dark:bg-dark-primary'>error</div>
       <TextField name='Email' type='email' placeholder={'Entermail@gmail.com'} />
-      {/* <TextField name='Phone Number' type='tel' placeholder={'+972 052 123-4567'} />
+      <TextField name='Phone Number' type='tel' placeholder={'+972 052 123-4567'} />
       <TextField name='Website' placeholder={'www.google.com'} />
       <TextField name='description' placeholder={'Enter a Description...'} />
-      <TextField type='textarea' placeholder={'Enter a Description 2...'} /> */}
+      <TextField type='textarea' placeholder={'Enter a Description 2...'} />
     </div>
   );
 }
