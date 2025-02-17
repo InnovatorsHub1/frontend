92c3d3923df46377a06ea1908866c85d95fd8ea7
benshabbat <benshabbat27@gmail.com>
2025-02-17 10:02:29 +0200

    fix style to figma with secondary and drakmode

--- src/components/Button/GenericButton.tsx
+++ src/components/Button/GenericButton.tsx
@@ -1,6 +1,7 @@
 import { Button as MuiButton } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import { ButtonProps } from './ButtonProps';
+import { brotliDecompress } from 'zlib';
 
 const VARIANT_STYLES = {
   primary: {
@@ -16,13 +17,14 @@ const VARIANT_STYLES = {
   },
   secondary: {
     backgroundColor: 'rgb(61, 61, 61)',
-    color: '#fff',
+    color: 'rgb(72, 143, 102)',
+    border: '2px solid rgb(72, 143, 102)',
     '&:hover': {
       backgroundColor: 'rgb(45, 45, 45)',
     },
     '&.Mui-disabled': {
       backgroundColor: 'rgba(61, 61, 61, 0.5)',
-      color: '#fff',
+      color: 'rgb(72, 143, 102)',
     },
   },
   outlined: {
@@ -38,6 +40,44 @@ const VARIANT_STYLES = {
       color: 'rgba(72, 143, 102, 0.5)',
     },
   },
+  dark: {
+    primary: {
+      backgroundColor: '#6FCF97',
+      color: '#11151F',
+      '&:hover': {
+        backgroundColor: 'rgba(111, 207, 151, 0.8)',
+      },
+      '&.Mui-disabled': {
+        backgroundColor: 'rgba(111, 207, 151, 0.5)',
+        color: '#11151F',
+      },
+    },
+    secondary: {
+      backgroundColor: '#3D3D3D',
+      color: '#6FCF97',
+      broder:'2px solid #6FCF97',
+      '&:hover': {
+        backgroundColor: 'rgb(45, 45, 45)',
+      },
+      '&.Mui-disabled': {
+        backgroundColor: 'rgba(61, 61, 61, 0.5)',
+        color: 'rgba(111, 207, 151, 0.5)',
+      },
+    },
+    outlined: {
+      border: '2px solid #6FCF97',
+      backgroundColor: 'transparent',
+      color: '#6FCF97',
+      '&:hover': {
+        backgroundColor: 'rgba(111, 207, 151, 0.05)',
+        border: '2px solid #6FCF97',
+      },
+      '&.Mui-disabled': {
+        border: '2px solid rgba(111, 207, 151, 0.5)',
+        color: 'rgba(111, 207, 151, 0.5)',
+      },
+    },
+  }
 } as const;
 
 export default function GenericButton(props: ButtonProps) {
