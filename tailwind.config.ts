25b15916d83b54157946d81175bbe2bf10f6f269
Shay Bushary <shaybush93@gmail.com>
2025-02-21 11:30:37 +0200

    splited lines

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from 'tailwindcss/types/config';
 import {
-  colors,screens,spacing,width,minWidth,maxWidth,height,
-  minHeight,maxHeight,borderWidth,borderRadius,boxShadow,saturate,contrast,
-  invert,translate,scale,backgroundImage,rotate,animation,keyframes,lineClamp,
+  colors, screens, spacing, width, minWidth, maxWidth, height,
+  minHeight, maxHeight, borderWidth, borderRadius, boxShadow, saturate, contrast,
+  invert, translate, scale, backgroundImage, rotate, animation, keyframes, lineClamp,
 } from './src/styles/constantsStyle';
 
 const tailwindConfig: Config = {
@@ -11,10 +11,28 @@ const tailwindConfig: Config = {
   darkMode: ['class', '[data-theme="dark"]'], // <--- from tests I made on Storybook, this array doesn't work. Only the data-theme="dark" affects the result, and the class does nothing. At first I thought may there's an AND behavior, but no, just the data attributes affects it. The class is rendered useless in this array form.
   theme: {
     screens,
-    extend: {spacing,width,minWidth,maxWidth,height,
-      minHeight,maxHeight,borderWidth,borderRadius,boxShadow,
-      saturate,contrast,invert,translate,scale,backgroundImage,rotate,
-      animation,keyframes,lineClamp,colors: colors,
+    extend: {
+      spacing,
+      width,
+      minWidth,
+      maxWidth,
+      height,
+      minHeight,
+      maxHeight,
+      borderWidth,
+      borderRadius,
+      boxShadow,
+      saturate,
+      contrast,
+      invert,
+      translate,
+      scale,
+      backgroundImage,
+      rotate,
+      animation,
+      keyframes,
+      lineClamp,
+      colors,
     },
   },
   corePlugins: {
