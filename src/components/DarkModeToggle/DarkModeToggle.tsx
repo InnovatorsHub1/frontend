10625962b2289bcac09bb3fe9ee854b7152c7d59
benshabbat <benshabbat27@gmail.com>
2025-02-24 18:50:29 +0200

    Refactor DarkModeToggle component for cleaner return statement

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -2,11 +2,9 @@ import { useDarkTheme } from '../../providers/DarkThemeProvider/DarkThemeContext
 import { ToggleButtonProps, ToggleButton } from '../ToggleButton/ToggleButton';
 
 export default function DarkModeToggle(props: ToggleButtonProps) {
-  const { size, className} = props;
+  const { size, className } = props;
 
   const { isDarkMode, toggleDarkMode } = useDarkTheme();
 
-  return (
-    <ToggleButton value={isDarkMode} setValue={toggleDarkMode} size={size} className={className} />
-  );
+  return <ToggleButton value={isDarkMode} setValue={toggleDarkMode} size={size} className={className} />;
 }
