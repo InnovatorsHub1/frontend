aac36bdead75b8e9482e18fb17f4e8e518435cf3
benshabbat <benshabbat27@gmail.com>
2025-02-24 13:35:04 +0200

    Refactor DarkThemeProvider component to streamline props definition and improve readability

--- src/providers/DarkThemeProvider/DarkThemeProvider.tsx
+++ src/providers/DarkThemeProvider/DarkThemeProvider.tsx
@@ -3,16 +3,15 @@ import { LS_KEY_THEME } from '../../utils/constants';
 import { DarkThemeContext, Theme } from './DarkThemeContext';
 import { useLocalStorage } from '@src/hooks/useLocalStorage';
 
-const THEME_OPTIONS: { dark: Theme; light: Theme } = { dark: 'dark', light: 'light' };
-
 type DarkThemeProviderProps = PropsWithChildren;
+
+const THEME_OPTIONS: { dark: Theme; light: Theme } = { dark: 'dark', light: 'light' };
 
 export default function DarkThemeProvider(props: DarkThemeProviderProps) {
   const { children } = props;
 
   const [localStorageTheme, setLocalStorageTheme] = useLocalStorage(LS_KEY_THEME);
 
-  // all useStates:
   const [isDarkMode, setIsDarkMode] = useState(() => {
     const deviceTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
       ? THEME_OPTIONS.dark
@@ -27,12 +26,10 @@ export default function DarkThemeProvider(props: DarkThemeProviderProps) {
     return currentTheme === THEME_OPTIONS.dark;
   });
 
-  // all functions:
   const toggleDarkMode = useCallback(() => {
     const themeToBe = isDarkMode ? THEME_OPTIONS.light : THEME_OPTIONS.dark;
     setLocalStorageTheme(themeToBe);
 
-    // @ts-ignore
     const [htmlElement] = document.getElementsByTagName('html');
     htmlElement!.setAttribute('data-theme', themeToBe);
     document.body.setAttribute('class', themeToBe);
