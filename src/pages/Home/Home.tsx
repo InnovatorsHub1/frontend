b9f11cd15c70f593a88a40a7682e56b642a793fa
benshabbat <benshabbat27@gmail.com>
2025-02-27 23:44:47 +0200

    Remove GenericButton and error div from HomePage component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,6 @@
-import GenericButton from '@src/components/Button';
-
 export default function HomePage() {
   return (
     <div className='size-full p-6'>
-      <div className=' bg-dark-primary hover:bg-hover-primary hover:text-pressed-primary'>error</div>
-
-      <GenericButton>Text</GenericButton>
     </div>
   );
 }
