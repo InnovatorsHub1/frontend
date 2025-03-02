8baf8b2dc639766bb0aeaa3d9d0dfbc56b1309ae
Sacha-Marciano <Momarciano26@gmail.com>
2025-03-02 20:33:04 +0200

    Add toggle dark mode to Storybook

--- src/components/form/DatePicker/DatePicker.stories.tsx
+++ src/components/form/DatePicker/DatePicker.stories.tsx
@@ -1,6 +1,8 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import DatePicker from './DatePicker';
-import { ComponentType, ReactNode } from 'react';
+import { ReactNode } from 'react';
+import DarkModeToggle from '@src/components/DarkModeToggle';
+import DarkThemeProvider from '@src/providers/DarkThemeProvider';
 
 const meta = {
   title: 'Date Picker',
@@ -8,23 +10,31 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
-  decorators: [(Story: ComponentType) => <Story />],
+  decorators: [
+    (Story) => (
+      <DarkThemeProvider>
+        <Story />
+      </DarkThemeProvider>
+    ),
+  ],
   tags: ['new', 'version:1.0.0'],
 } satisfies Meta<typeof DatePicker>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    children: 'example',
-    type: 'default',
-  },
+export const Default = (): ReactNode => {
+  return (
+    <div className='flex gap-2'>
+      <DarkModeToggle size={20} />
+      <DatePicker />
+    </div>
+  );
 };
 
 export const AllDatePickers = (): ReactNode => {
   return (
     <div className='flex gap-2'>
+      <DarkModeToggle size={20} />
       <DatePicker />
       <DatePicker label='Choose a date' />
       <DatePicker value={new Date()} />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,9 @@
+import DatePicker from '@src/components/form/DatePicker';
+
 export default function HomePage() {
-  return <div className='size-full p-6'></div>;
+  return (
+    <div className='size-full p-6'>
+      <DatePicker />
+    </div>
+  );
 }
