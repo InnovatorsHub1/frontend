76377df56bf8d25a4d539a5ffcb8110d90cb38c2
Sacha-Marciano <Momarciano26@gmail.com>
2025-03-02 20:47:17 +0200

    fix conflicts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,3 @@
-import DatePicker from '@src/components/form/DatePicker';
-
 export default function HomePage() {
-  return (
-    <div className='size-full p-6'>
-      <DatePicker />
-    </div>
-  );
+  return <div className='size-full p-6'> </div>;
 }
