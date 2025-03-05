f5391c7a33d133bf7cdc045a1913e5c1e6d96422
benshabbat <benshabbat27@gmail.com>
2025-03-05 09:57:20 +0200

    Refactor useCookies hook to update cookie management methods

--- src/hooks/useCookies.ts
+++ src/hooks/useCookies.ts
@@ -7,7 +7,7 @@ const getFullKey = (key: string) => `${prefix}__${key}`;
 export function useCookies<T = any>(key: string, defaultValue?: T) {
   const fullKey = getFullKey(key);
   const [value, setValue] = useState<T>(() => {
-    const item = cookies.getItem(fullKey);
+    const item = cookies.get(fullKey);
 
     if (item) {
       try {
@@ -21,10 +21,10 @@ export function useCookies<T = any>(key: string, defaultValue?: T) {
   });
 
   useEffect(() => {
-    cookies.setItem(fullKey, JSON.stringify(value));
+    cookies.set(fullKey, JSON.stringify(value));
   }, [key, value]);
 
-  const deleteValue = useCallback(() => cookies.removeItem(fullKey), [key]);
+  const deleteValue = useCallback(() => cookies.remove(fullKey), [key]);
 
   return [value, setValue, deleteValue] as [T, React.Dispatch<React.SetStateAction<T>>, () => void];
 }
