c898645ec3ddfdde89d1eba271cad7ffa53d8575
benshabbat <benshabbat27@gmail.com>
2025-03-06 22:11:35 +0200

    Add GenericButton component to HomePage with Tailwind styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,10 @@
+import GenericButton from '@src/components/Button';
+
 export default function HomePage() {
   return (
     <div className='size-full p-6'>
       <div className=' bg-dark-primary hover:bg-hover-primary hover:text-pressed-primary'>error</div>
+      <GenericButton className='bg-blue-500 dark:bg-red-500'>button of mui with tailwind</GenericButton>
     </div>
   );
 }
