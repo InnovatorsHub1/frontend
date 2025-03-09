f4e5e42f302acda01f6d6a475c3e3f5800635260
David Chen Benshabbat <102465686+benshabbat@users.noreply.github.com>
2025-03-09 10:48:48 +0200

    Fs 75 Button (#83)
    * Refactor button component to use theme-based styles and remove variant prop
    
    * Refactor button component to use theme-based styles and remove variant prop
    
    * Refactor GenericButton stories and tests to remove variant prop and update styles
    
    * Refactor GenericButton stories and tests to remove variant prop and update styles
    
    * Refactor GenericButton component to standardize size prop values and simplify props destructuring
    
    * added rest for sx
    
    * Refactor theme handling in GenericButton and DarkModeToggle components to use updated DarkThemeContext and improve prop management
    
    * Refactor DarkThemeProvider component to streamline props definition and improve readability
    
    * Remove unused color assertions from GenericButton tests
    
    * Remove useTheme hook to simplify theme management
    
    * Remove GenericButton and error div from HomePage component
    
    * Simplify HomePage component by removing unnecessary return statement formatting
    
    * Add DarkThemeProvider decorator to Button stories for consistent theming
    
    * Refactor button theme styles for improved readability and maintainability
    
    * Update Button stories to include tags and adjust CSS for full-width layout
    
    * Remove default value for loading prop in GenericButton component
    
    * Refactor Button stories to remove redundant DarkModeToggle instances and simplify structure
    
    * Remove unused SaveIcon import from Button stories
    
    * Refactor GenericButton tests to use default props and simplify rendering
    
    * Refactor Button stories to use StoryObj type and simplify defaultButton export
    
    * Refactor GenericButton to use dynamic theme styles and simplify theme handling

--- src/components/Button/GenericButton.tsx
+++ src/components/Button/GenericButton.tsx
@@ -1,56 +1,31 @@
 import { Button as MuiButton } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import { ButtonProps } from './ButtonProps';
-import { VARIANT_STYLES } from './constants';
-
+import { THEME_STYLES } from './constants';
+import { useDarkTheme } from '@src/providers/DarkThemeProvider/DarkThemeContext';
 export default function GenericButton(props: ButtonProps) {
-  const {
-    variant = 'primary',
-    size = 'md',
-    children,
-    iconPosition = 'left',
-    loading = false,
-    className = '',
-    icon,
-    disabled = false,
-  } = props;
-  const muiSize = size === 'sm' ? 'small' : size === 'lg' ? 'large' : 'medium';
-  const muiVariant = variant === 'outlined' ? 'outlined' : 'contained';
+  const { icon, loading, children, disabled, iconPosition = 'left' } = props;
 
-  const combinedClassName = [className, loading ? 'loading' : ''].filter(Boolean).join(' ');
-  const buttonStyles = {
-    textTransform: 'none',
-    borderRadius: '6px',
-    boxShadow: 'none',
-    fontWeight: 500,
-    position: 'relative',
-    ...VARIANT_STYLES[variant],
-    '&:hover': {
-      boxShadow: 'none',
-      ...VARIANT_STYLES[variant]['&:hover'],
-    },
-  };
-
+  const { theme } = useDarkTheme();
+  const styleButton = THEME_STYLES(theme);
   const loadingIndicator = loading && (
     <CircularProgress
       size={20}
       color='inherit'
       sx={{
         marginRight: children ? '8px' : 0,
-        color: variant === 'outlined' ? 'rgb(72, 143, 102)' : 'inherit',
+        color: 'inherit',
       }}
     />
   );
+
   return (
     <MuiButton
       {...props}
-      variant={muiVariant}
-      size={muiSize}
       disabled={disabled || loading}
       startIcon={iconPosition === 'left' && !loading ? icon : undefined}
       endIcon={iconPosition === 'right' && !loading ? icon : undefined}
-      className={combinedClassName}
-      sx={buttonStyles}
+      sx={{ ...styleButton, ...props.sx }}
     >
       {loadingIndicator}
       {children}
--- src/components/Button/__tests__/GenericButton.test.tsx
+++ src/components/Button/__tests__/GenericButton.test.tsx
@@ -1,40 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import GenericButton from '../GenericButton';
 
 describe('GenericButton Component', () => {
+  const defaultProps = {
+    children: 'TEST',
+    disabled: true,
+  };
+
   beforeEach(() => {
-    jest.clearAllMocks();
-    render(
-      <>
-        <GenericButton>TEST</GenericButton>
-        <GenericButton variant='primary'>Primary Button</GenericButton>
-        <GenericButton disabled>Disabled Button</GenericButton>
-      </>,
-    );
+    render(<GenericButton {...defaultProps}>{defaultProps.children}</GenericButton>);
   });
 
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
   it('renders the button with correct text', () => {
-    const buttonElement = screen.getByRole('button', { name: /TEST/i });
+    const buttonElement = screen.getByRole('button');
     expect(buttonElement).toBeInTheDocument();
   });
 
   it('renders primary button correctly', () => {
-    const button = screen.getByRole('button', { name: 'Primary Button' });
+    const button = screen.getByRole('button');
     expect(button).toHaveClass('MuiButton-root');
-    expect(button.closest('button')).toHaveStyle('text-transform: none');
-    expect(button).toHaveClass('MuiButton-contained');
+    expect(button.closest('button'));
   });
 
   it('renders disabled button correctly', () => {
-    const button = screen.getByRole('button', { name: 'Disabled Button' });
+    const button = screen.getByRole('button');
     expect(button).toBeDisabled();
-    expect(button).toHaveClass('Mui-disabled');
-    expect(button).toHaveStyle({
-      backgroundColor: 'rgba(72, 143, 102, 0.5)',
-    });
   });
 });
