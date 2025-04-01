f40b3d8ed238ef20d4f0443a28c433d3110a41fd
David Chen Benshabbat <102465686+benshabbat@users.noreply.github.com>
2025-04-01 11:27:09 +0300

    Fs 84 Tailwind configue with mui (#89)
    * Update tailwindcss to version 3.4.17 in package.json and pnpm-lock.yaml
    
    * Add important and preflight options to Tailwind CSS configuration
    
    * Add GenericButton component to HomePage with Tailwind styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import GenericButton from '@src/components/Button';
+
 export default function HomePage() {
   return <h1>Home</h1>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const tailwindConfig: Config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   // safelist: [{ pattern: /^(bg-|border-|text-)/, variants: ['hover', 'active'] }, 'bg-red-200'],
   darkMode: ['class', '[data-theme="dark"]'], // <--- from tests I made on Storybook, this array doesn't work. Only the data-theme="dark" affects the result, and the class does nothing. At first I thought may there's an AND behavior, but no, just the data attributes affects it. The class is rendered useless in this array form.
+  important: '#root',
   theme: {
     screens,
     extend: {
@@ -37,6 +38,7 @@ const tailwindConfig: Config = {
   },
   corePlugins: {
     aspectRatio: false, // disable the aspectRatio core plugin to avoid conflicts with the native aspect-ratio utilities included in Tailwind CSS v3.0
+    preflight: false, // disable the preflight core plugin to avoid conflicts with the native preflight styles included in Tailwind CSS v3.0
   },
   variants: {
     animation: ({ after }) => after(['motion-safe', 'motion-reduce']),
